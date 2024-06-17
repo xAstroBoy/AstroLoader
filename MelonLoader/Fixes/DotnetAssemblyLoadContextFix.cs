cc45e0ea1d994e211a3e4172218d9a207d1fa133
Trevor <me@trev.app>
2024-06-17 17:36:58 -0400

    Minor core cleanup

--- MelonLoader/Fixes/DotnetAssemblyLoadContextFix.cs
+++ MelonLoader/Fixes/DotnetAssemblyLoadContextFix.cs
@@ -34,6 +34,7 @@ namespace MelonLoader.Fixes
                 AssemblyVerifier.EnsureInitialized();
 
                 //Now hook ALC.
+                // TODO: these don't exist in .NET 8, i don't know how important this is
                 Core.HarmonyInstance.Patch(AlcQCallLoadFromPath, new HarmonyMethod(typeof(DotnetAssemblyLoadContextFix), nameof(PreAlcLoadFromPath)));
                 Core.HarmonyInstance.Patch(AlcQCallLoadFromStream, new HarmonyMethod(typeof(DotnetAssemblyLoadContextFix), nameof(PreAlcLoadFromStream)));
             }
