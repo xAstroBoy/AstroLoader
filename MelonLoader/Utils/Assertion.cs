d98d23e5c0dd144bd0173486ce1c0e0d33a5d7f6
Trevor <me@trev.app>
2024-06-20 11:09:34 -0400

    Remove MessageBox on Internal Failure

--- MelonLoader/Utils/Assertion.cs
+++ MelonLoader/Utils/Assertion.cs
@@ -8,11 +8,6 @@ namespace MelonLoader.Utils
     {
         internal static bool ShouldContinue = true;
 
-        //TODO: Could this be done in a better way? net35/6 load PresentationFramework differently so I could not rely on it
-        //This crashes with start screen enabled
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        internal static extern IntPtr MessageBox(int hWnd, String text, String caption, uint type);
-
         internal static void ThrowInternalFailure(string msg)
         {
             if (!ShouldContinue)
@@ -22,11 +17,7 @@ namespace MelonLoader.Utils
 
             var timestamp = LoggerUtils.GetTimeStamp();
             MelonLogger.WriteLogToFile($"[{timestamp}] [INTERNAL FAILURE] {msg}");
-
-            string caption = "INTERNAL FAILURE!";
-            var result = MessageBox(0, msg, caption, 0);
-            while (result == IntPtr.Zero)
-                Environment.Exit(1);
+            Environment.Exit(1);
         }
     }
 }
