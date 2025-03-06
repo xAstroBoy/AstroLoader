9fa1a29fce9c379e5ee7df8365c85bc17cc6d7ce
aldelaro5 <aldelaro5@gmail.com>
2025-03-06 16:26:12 -0500

    Add utilities for Linux in LibcNative

--- MelonLoader.Bootstrap/LibcNative.cs
+++ MelonLoader.Bootstrap/LibcNative.cs
@@ -1,10 +1,16 @@
 #if LINUX
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace MelonLoader.Bootstrap;
 
-public partial class LibcNative
+internal partial class LibcNative
 {
+    internal const int Stdout = 1;
+    internal const int Stderr = 2;
+
+    internal const int SeekEnd = 2;
+    
     [LibraryImport("libc", EntryPoint = "__libc_start_main")]
     public static unsafe partial int LibCStartMain(
         delegate* unmanaged[Cdecl]<int, char**, char**, int> main,
@@ -16,6 +22,39 @@ public partial class LibcNative
         nint stackEnd);
 
     [LibraryImport("libc", EntryPoint = "setenv", StringMarshalling = StringMarshalling.Utf8)]
-    public static unsafe partial int Setenv(string name, string value,[MarshalAs(UnmanagedType.Bool)] bool overwrite);
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int Setenv(string name, string value,[MarshalAs(UnmanagedType.Bool)] bool overwrite);
+
+    [LibraryImport("libc", EntryPoint = "dup2")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int Dup2(int oldFd, int newFd);
+
+    [LibraryImport("libc", EntryPoint = "fopen64", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial nint Fopen64(string pathName, string modes);
+
+    [LibraryImport("libc", EntryPoint = "vfprintf", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int Vfprintf(nint stream, string format, nint vList);
+
+    [LibraryImport("libc", EntryPoint = "vsnprintf", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static unsafe partial int Vsnprintf(byte* s, int maxLen, string format, nint arg);
+
+    [LibraryImport("libc", EntryPoint = "fseek", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int Fseek(nint stream, long offset, int whence);
+
+    [LibraryImport("libc", EntryPoint = "fwrite", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static unsafe partial int Fwrite(byte* ptr, int size, int nItems, nint stream);
+
+    [LibraryImport("libc", EntryPoint = "fileno")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int Fileno(nint stream);
+
+    [LibraryImport("libc", EntryPoint = "fclose")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial int FClose(nint stream);
 }
 #endif
