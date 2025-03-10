30bbffeb7b9c69f9f20f29bac2660377908a1f71
aldelaro5 <aldelaro5@gmail.com>
2025-03-09 23:54:09 -0400

    Add a custom post build task that converts ppdb to mdb to support net35 mono debugging

---
+++ PortablePdbToMdb/PortablePdbToMdb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using Mono.Cecil.Mdb;
+
+namespace PortablePdbToMdb;
+
+public class PortablePdbToMdb : Task
+{
+    [Required]
+    public string? AssemblyPath { get; set; }
+    
+    public override bool Execute()
+    {
+        try
+        {
+            var module = ModuleDefinition.ReadModule(AssemblyPath, new()
+            {
+                ReadSymbols =  true,
+            });
+
+            var allMethod = module.GetTypes().SelectMany(x => x.Methods);
+            var writerProvider = new MdbWriterProvider();
+            using (var writer = writerProvider.GetSymbolWriter(module, AssemblyPath))
+            {
+                foreach (MethodDefinition methodDefinition in allMethod)
+                    writer.Write(methodDefinition.DebugInformation);
+            }
+        }
+        catch (Exception e)
+        {
+            Log.LogError(e.Message);
+            return false;
+        }
+
+        string mdbPath = AssemblyPath + ".mdb";
+        if (File.Exists(mdbPath))
+            Log.LogMessage(MessageImportance.High, $"Mdb file created successfully at {mdbPath}");
+        return true;
+    }
+}
