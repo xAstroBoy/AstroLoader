5e3d7136a501939d2be632805e26975cb67d9aa2
Herp Derpinstine <herpderpinstine@gmail.com>
2025-03-27 17:43:21 -0600

    Fixed MelonModule Deletion Conflict

--- MelonLoader/CompatibilityLayers/MelonCompatibilityLayer.cs
+++ MelonLoader/CompatibilityLayers/MelonCompatibilityLayer.cs
@@ -64,21 +64,7 @@ namespace MelonLoader
             CheckGameLayer($"{InternalUtils.UnityInformationHandler.GameDeveloper}_{InternalUtils.UnityInformationHandler.GameName}");
 
             foreach (var m in layers)
-            {
-                if ((m.shouldBeIgnored != null)
-                    && m.shouldBeIgnored())
-                    continue;
-
-                MelonDebug.Msg($"Loading MelonModule '{m.fullPath}'");
                 m.moduleGC = MelonModule.Load(m);
-            }
-
-            foreach (var file in Directory.GetFiles(baseDirectory))
-            {
-                string fileName = Path.GetFileName(file);
-                if (layers.Find(x => Path.GetFileName(x.fullPath).Equals(fileName)) == null)
-                    File.Delete(file);
-            }
         }
     }
 }
